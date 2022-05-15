3dbcc91171fef177f85a300b8899fad19da0508a
Kasowy-Code <maciej.smirski@hotmail.com>
2022-05-15 19:50:59 +0200

    reworked hard drop, fixed tetromino tearing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <map>
+#include <vector>
 
 const unsigned int TileSize = 32;
 const unsigned int PlayAreaWidth = 10;
 const unsigned int PlayAreaHeight = 20;
+bool isDropping = true;
 
 class Mino;
 
@@ -27,9 +29,12 @@ public:
     int getPositionX() { return this->X; }
     int getPositionY() { return this->Y; }
     sf::Sprite block;
+
+    void setPositionX(int X) { this->X = X; }
+    void setPositionY(int Y) { this->Y = Y; }
 private:
-    int X;
-    int Y;
+    int X = 0;
+    int Y = 0;
 };
 
 Mino::Mino(Colors color, int X, int Y, const sf::Texture &texture)
@@ -43,11 +48,15 @@ Mino::Mino(Colors color, int X, int Y, const sf::Texture &texture)
 
 
 void Mino::Move(Directions direction) {
+    bool finished = false;
     switch (direction) {
     case down:
-        if (this->Y < PlayAreaHeight-1) {
+        if (MapPlaces[this->getPositionX()][this->getPositionY() + 1] == false && this->getPositionY() != PlayAreaHeight - 1) {
             this->Y++;
             block.setPosition(sf::Vector2f(this->X * 32.0f, this->Y * 32.0f));
+        }
+        else {
+            throw -1;
         }
         break;
     case right:
@@ -56,7 +65,7 @@ void Mino::Move(Directions direction) {
             block.setPosition(sf::Vector2f(this->X * 32.0f, this->Y * 32.0f));
         }
         else {
-            throw ("error");
+            throw("right");
         }
         break;
     case left:
@@ -65,37 +74,155 @@ void Mino::Move(Directions direction) {
             block.setPosition(sf::Vector2f(this->X * 32.0f, this->Y * 32.0f));
         }
         else {
-            throw ("Error");
+            throw("left");
         }
         break;
-    case hardDrop:
-        for (int i = this->getPositionY(); i < 20; i++) {
-                if (MapPlaces[this->getPositionX()][i+1] == true || i == 19) {
-                    std::cout << "I: " << i << std::endl;
-                    this->Y = i;
-                    MapPlaces[this->getPositionX()][this->getPositionY()] = true;
-                    break;
-                }
-            
-        }
-        block.setPosition(sf::Vector2f(this->X*32.0f, this->Y*32.0f));
-        break;
+    //case hardDrop:
+    //    /*for (int i = this->getPositionY(); i < 20; i++) {
+    //            if (MapPlaces[this->getPositionX()][i+1] == true || i == 19) {
+    //                std::cout << "I: " << i << std::endl;
+    //                this->Y = i;
+    //                Minos.push_back(this);
+    //                MapPlaces[this->getPositionX()][this->getPositionY()] = true;
+    //                break;
+    //            }
+    //        
+    //    }*/
+
+    //    while (!finished) {
+    //        if (MapPlaces[this->getPositionX()][this->getPositionY() + 1] == false && this->getPositionY() != PlayAreaHeight - 1) {
+    //            this->Y++;
+    //            block.setPosition(sf::Vector2f(this->X * 32.0f, this->Y * 32.0f));
+    //        }
+    //        else {
+    //            finished = true;
+    //            Minos.push_back(this);
+    //                MapPlaces[this->getPositionX()][this->getPositionY()] = true;
+    //        }
+    //    }
+    //    block.setPosition(sf::Vector2f(this->X*32.0f, this->Y*32.0f));
+    //    break;
     }
 }
 
-//class Tetromino
+class Tetromino
+{
+public:
+    Tetromino(Colors color, const sf::Texture& texture);
+    Tetromino() {};
+    void Move(Directions direction);
+    //virtual ~Tetromino() {}
+    std::vector<Mino*> blocks;
+};
+
+Tetromino::Tetromino(Colors color, const sf::Texture& texture)
+{
+    for (int i = 0; i < 4; i++) {
+        Mino* el = new Mino(color, -1, -1, texture);
+        el->block.setTexture(texture);
+        el->block.setTextureRect(sf::IntRect(color * 32, 0, 32, 32));
+        blocks.push_back(el);
+    }
+
+}
+void Tetromino::Move(Directions direction) {
+    std::vector<Mino> saveState;
+    for (const auto& el : blocks) {
+        saveState.push_back(*el);
+    }
+    if (direction == hardDrop) {
+        try {
+            while (true) {
+
+                for (const auto &el : blocks) {
+                    saveState.push_back(*el);
+                }
+                for (auto& el : blocks) {
+                    el->Move(down);
+                }
+                saveState.clear();
+            }
+        }
+        catch (int e) {
+            for (int i = 0; i < 4; i++) {
+                blocks[i]->setPositionX(saveState[i].getPositionX());
+                blocks[i]->setPositionY(saveState[i].getPositionY());
+                blocks[i]->block.setPosition(sf::Vector2f(blocks[i]->getPositionX() * 32.0f, blocks[i]->getPositionY() * 32.0f));
+            }
+            isDropping = false;
+            for (auto& el : this->blocks) {
+                Minos.push_back(el);
+                MapPlaces[el->getPositionX()][el->getPositionY()] = true;
+            }
+        }
+        
+    }
+    else {
+        try {
+            for (auto& el : blocks) {
+                el->Move(direction);
+            }
+        }
+        catch (int e) {
+            for (int i = 0; i < 4; i++) {
+                blocks[i]->setPositionX(saveState[i].getPositionX());
+                blocks[i]->setPositionY(saveState[i].getPositionY());
+                blocks[i]->block.setPosition(sf::Vector2f(blocks[i]->getPositionX() * 32.0f, blocks[i]->getPositionY() * 32.0f));
+            }
+            isDropping = false;
+            for (auto& el : this->blocks) {
+                Minos.push_back(el);
+                MapPlaces[el->getPositionX()][el->getPositionY()] = true;
+            }
+        }
+        catch (...) {
+            for (int i = 0; i < 4; i++) {
+                blocks[i]->setPositionX(saveState[i].getPositionX());
+                blocks[i]->setPositionY(saveState[i].getPositionY());
+                blocks[i]->block.setPosition(sf::Vector2f(blocks[i]->getPositionX() * 32.0f, blocks[i]->getPositionY() * 32.0f));
+            }
+
+        }
+    }
+}
+
+class LShape : public Tetromino
+{
+public:
+    LShape(Colors color, const sf::Texture& texture) : Tetromino{color, texture} {
+
+        this->blocks[0]->setPositionX(4);
+        this->blocks[0]->setPositionY(0);
+
+        this->blocks[1]->setPositionX(5);
+        this->blocks[1]->setPositionY(0);
+
+        this->blocks[2]->setPositionX(4);
+        this->blocks[2]->setPositionY(-1);
+
+         this->blocks[3]->setPositionX(4);
+        this->blocks[3]->setPositionY(-2);
+
+        for (auto& el : blocks) {
+            el->block.setTexture(texture);
+            el->block.setTextureRect(sf::IntRect(color * 32, 0, 32, 32));
+            el->block.setPosition(sf::Vector2f(el->getPositionX() * 32.0f, el->getPositionY() * 32.0f));
+        }
+    }
+   // ~LShape();
+
+private:
+
+};
+
+//LShape::LShape(Colors color, const sf::Texture& texture) : Tetromino()
 //{
-//public:
-//    Tetromino();
-//    //virtual ~Tetromino();
 //
-//private:
-//
-//};
-//
-//Tetromino::Tetromino()
-//{
+//   
+//    
 //}
+
+
 
 
 int main()
@@ -103,21 +230,20 @@ int main()
     sf::Texture texture;
     texture.loadFromFile("../../assets/TileTextures.png");
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Tetris");
-    bool isDropping = true;
-    //Mino* test = new Mino(purple, 5, 7, texture);
-    //MapPlaces[5][7] = true;
-    //Minos.push_back(test);
+    Mino* test = new Mino(purple, 5, 7, texture);
+    MapPlaces[5][7] = true;
+    Minos.push_back(test);
     sf::Clock clock;
     int count = 0;
     while (window.isOpen())
     {
         count++;
-        Mino* mino;
+        LShape* mino;
         if (count % 2 == 0) {
-            mino = new Mino(red, 0, 0, texture);
+            mino = new LShape(red, texture);
         }
         else {
-            mino = new Mino(green, 0, 0, texture);
+            mino = new LShape(green, texture);
         }
         isDropping = true;
         while (isDropping) {
@@ -126,16 +252,9 @@ int main()
             sf::Event event;
             sf::Time time = clock.getElapsedTime();
             if (time.asMilliseconds() >= 400) {
-                if(MapPlaces[mino->getPositionX()][mino->getPositionY() + 1] == false && mino->getPositionY() != PlayAreaHeight-1) {
-                    mino->Move(down);
-                }
-                else {
-                    isDropping = false;
-                    Minos.push_back(mino);
-                    MapPlaces[mino->getPositionX()][mino->getPositionY()] = true;
-                }
+                mino->Move(down);
                 clock.restart();
-                std::cout << mino->getPositionY() << " " << mino->getPositionX() << std::endl;
+                std::cout << mino->blocks[0]->getPositionY() << " " << mino->blocks[0]->getPositionX() << std::endl;
             }
             while (window.pollEvent(event))
             {
@@ -147,14 +266,7 @@ int main()
                 if (event.type == event.KeyPressed) {
                     switch (event.key.code) {
                     case sf::Keyboard::S:
-                        if (MapPlaces[mino->getPositionX()][mino->getPositionY() + 1] == false && mino->getPositionY() != 20) {
-                            mino->Move(down);
-                        }
-                        else {
-                            isDropping = false;
-                            Minos.push_back(mino);
-                            MapPlaces[mino->getPositionX()][mino->getPositionY()] = true;
-                        }
+                        mino->Move(down);                       
                         break;
                     case sf::Keyboard::D:
                         try {
@@ -171,16 +283,15 @@ int main()
                     case sf::Keyboard::Space:
                         mino->Move(hardDrop);
                         isDropping = false;
-                        Minos.push_back(mino);
-                        MapPlaces[mino->getPositionX()][mino->getPositionY()] = true;
                         break;
                     }
                     clock.restart();
                     shouldPause = true;
                 }
             }
-
-            window.draw(mino->block);
+            for (auto el : mino->blocks) {
+                window.draw(el->block);
+            }
 
             for (auto el : Minos) {
                 window.draw(el->block);  
